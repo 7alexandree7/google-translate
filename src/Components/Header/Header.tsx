94ed23d22132aecb7733334be00710ff5c5d10ae
alexandre <alexandre020602@gmail.com>
2025-07-11 19:18:03 -0300

    finalizando o Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <header className="bg-white shadow-sm">
+            <div className="max-w-7xl mx-auto py-4 px-4 flex items-center">
+                <h1 className="text-[#3C4043] text-2xl font-semibold">Tradutor de Texto Unfast</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
