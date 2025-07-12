39bdf5817b14cb1984064e4ea5b9805329b352e3
alexandre <alexandre020602@gmail.com>
2025-07-12 13:41:17 -0300

    finalizando o footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer className='w-full bg-white px-4 py-2 mb-1'>
+            <div>
+                <p>&copy; {new Date().getFullYear()} Tradutor de Texto Unfast</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
